3ef49d84c697af32c3784f8b1d1e0a99c92a15cb
leonardo Vargas <lv9334889@gmail.com>
2026-02-02 01:06:18 -0300

    creando estructura del frontend

---
+++ client/src/views/auth/LoginView.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+
+import { ref } from 'vue';
+
+
+
+const NOMBRE = ref('')
+const EMAIL = ref('')
+const PASSWORD = ref('')
+
+
+
+const loginform = () => {
+
+}
+
+
+
+</script>
+
+
+
+
+<template>
+    <h1>LOGINFORM</h1>
+
+
+    <form @submit.prevent="loginform">
+
+
+
+        <label for="text">Nombre de Trabajador:</label>
+        <input v-model="NOMBRE" type="text">
+
+
+
+        <label for="text">Correo electronico:</label>
+        <input v-model="EMAIL" type="text">
+
+
+
+        <label for="password">Contraseña:</label>
+        <input v-model="PASSWORD" type="password">
+
+
+
+        <button type="submit">Ingresar</button>
+
+        <p>AQUI SE SUPONE QUE NO SE PUEDE REGISTRAR SOLO EL TRABAJADOR
+            TIENE QUE ESPERAR QUE EL ADMIN LOGEE AL TRABAJADOR ESO HACE QUE 
+            SEA MAS SEGURO 
+        </p>
+    </form>
+
+
+</template>
